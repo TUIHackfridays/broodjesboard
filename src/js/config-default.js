337b62448bf946267a15877e4cf054e9da863c1e
Koen Betsens <koen.betsens@gmail.com>
2016-04-11 14:21:45 +0200

    no more global config

--- src/js/config-default.js
+++ src/js/config-default.js
@@ -2,8 +2,7 @@ define({
   appid: '-',
   appname: 'Broodjes Board',
   apiversion: '1',
-  apiurl: 'js/dummy/',
-  authurl: 'http://api.corpfy.be/oauth2/',
-  publicurl: 'http://platform.corpfy.be',
-  gearmanurl: 'http://gearman.corpfy.be'
+  apiurl: 'http://broodjesapi.tuihackfridays.com/',
+  authurl: 'http://broodjesapi.tuihackfridays.com/oauth2/',
+  publicurl: 'http://broodjesapp.tuihackfridays.com'
 });
--- src/js/config.js
+++
@@ -1,9 +0,0 @@
-define({
-  appid: '-',
-  appname: 'Broodjes Board',
-  apiversion: '1',
-  apiurl: 'js/dummy/',
-  authurl: 'http://api.corpfy.be/oauth2/',
-  publicurl: 'http://platform.corpfy.be',
-  gearmanurl: 'http://gearman.corpfy.be'
-});
